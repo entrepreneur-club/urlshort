dadf2a2b5b31a87d9290769d0849d144599cdb6a
Jonathan Mueller <j.mueller@apoveda.ch>
2015-04-29 01:16:33 +0200

    comments

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
-define('DEFAULT_URL', 'http://entrepreneur-club.org/');
-define('NOTFOUND_URL', 'http://entrepreneur-club.org/notfound');
 
+# DEFINE NEW URLS HERE
 $urls = array(
+    # startup speed dating
     'ssd' => 'http://ssd.entrepreneur-club.org/',
+    # ec blog
     'blog' => 'http://blog.entrepreneur-club.org/'
 );
+
+# CHANGE NOTHING BELOW HERE
+
+define('DEFAULT_URL', 'http://entrepreneur-club.org/');
+define('NOTFOUND_URL', 'http://entrepreneur-club.org/notfound');
 
 $key = $_GET['url'];
 if(array_key_exists($key, $urls)) {
