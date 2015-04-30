3cba737f7db3273e2f8db70549edfb13fb8c72e2
Jonathan Mueller <j.mueller@apoveda.ch>
2015-04-30 15:12:19 +0200

    new url

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ $urls = array(
     # ec blog
     'blog' => 'http://blog.entrepreneur-club.org/',
     # github page
-    'git' => 'https://github.com/entrepreneur-club/'
+    'git' => 'https://github.com/entrepreneur-club/',
+    # Ideation - Mobility & Transport Workshop
+    'idea' => 'https://docs.google.com/forms/d/1Y8fR-iir_rnC6xXmhckj2rMfgSO8K8lcwNoKwHjUa4E/viewform'
 );
 
 # CHANGE NOTHING BELOW HERE
